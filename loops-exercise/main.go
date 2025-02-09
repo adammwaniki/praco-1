c15b5edf8f084b801d1a4b6a787c8b61dcb2bb1e
Adam Mwaniki <adamndegwa@protonmail.com>
2025-02-09 22:31:28 +0300

    Adds code execution time measurement

--- loops-exercise/main.go
+++ loops-exercise/main.go
@@ -1,18 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // Sqrt calculates the square root of a number using Newton's method.
 func sqrt(x float64) float64{
-	z:= float64(1) // Initial guess
+	startTime := time.Now() // Start time measurement
+	var z float64 = 1 // Initial guess
 	fmt.Printf("Sqrt of %v\n", x)
 	for i :=1; i<=10; i++{ // 10 iterations
 		z -= (z*z - x) / (2*z) // Newton's method
 		fmt.Printf("Iteration %v: %v\n", i, z)
 	}
+	elapsedTime := time.Since(startTime) // End time measurement and calculate elapsed time
+	fmt.Printf("Execution time: %v\n", elapsedTime)
 	return z
 }
 
 func main(){
-	fmt.Println(sqrt(0.125))
+	fmt.Println(sqrt(0.125), sqrt(2), sqrt(4), sqrt(16))
 }
