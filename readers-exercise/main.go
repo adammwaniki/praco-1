6efa4a04165b06e2f3025b74d2d88e80ce445708
Adam Mwaniki <adamndegwa@protonmail.com>
2025-03-12 14:27:22 +0300

    Completes the exercise

--- readers-exercise/main.go
+++ readers-exercise/main.go
@@ -1,11 +1,23 @@
 package main
 
-import "golang.org/x/tour/reader"
+import (
+	"golang.org/x/tour/reader"
+)
 
+// Define MyReader type
 type MyReader struct{}
 
-// TODO: Add a Read([]byte) (int, error) method to MyReader.
+// Implement the Read method for MyReader
+func (mr MyReader) Read(b []byte) (int, error) {
+	// Fill the provided slice with 'A'
+	for i := range b {
+		b[i] = 'A'
+	}
+	// Return the number of bytes filled and no error
+	return len(b), nil
+}
 
 func main() {
+	// Validate that MyReader implements io.Reader correctly
 	reader.Validate(MyReader{})
 }
